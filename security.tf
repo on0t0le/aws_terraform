1dbf9ae4f461d96f416ff0ed7ad17a3b24929c7a
unknown <bugrovka12@ukr.net>
2019-07-14 18:33:43 +0300

    Fix playbooks

--- security.tf
+++ security.tf
@@ -9,6 +9,13 @@ resource "aws_security_group" "ssh" {
     protocol    = "tcp"
     cidr_blocks = ["${var.myIP}"]
   }
+  ### Allow outgoing traffic
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 
   tags = {
     Name = "Allow ssh k8s"
